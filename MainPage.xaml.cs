0dd7be4750975bcfbbbbbf3c1d6c5ea71c072cb7
YehonatanKadosh <jacojacoj3@gmail.com>
2021-03-17 17:09:05 +0200

    Add project files.

---
+++ MainPage.xaml.cs
@@ -0,0 +1,56 @@
+﻿using System;
+using System.Collections.Generic;
+using System.IO;
+using System.Linq;
+using System.Runtime.InteropServices.WindowsRuntime;
+using Windows.Foundation;
+using Windows.Foundation.Collections;
+using Windows.UI.Xaml;
+using Windows.UI.Xaml.Controls;
+using Windows.UI.Xaml.Controls.Primitives;
+using Windows.UI.Xaml.Data;
+using Windows.UI.Xaml.Input;
+using Windows.UI.Xaml.Media;
+using Windows.UI.Xaml.Navigation;
+
+// The Blank Page item template is documented at https://go.microsoft.com/fwlink/?LinkId=402352&clcid=0x409
+
+namespace DataStracturesProject
+{
+    /// <summary>
+    /// An empty page that can be used on its own or navigated to within a Frame.
+    /// </summary>
+    public sealed partial class MainPage : Page
+    {
+        public MainPage()
+        {
+            this.InitializeComponent();
+        }
+        //
+        //class Program
+        //{
+        //    static Logic _logic = new Logic(10000);
+        //    static void Main(string[] args)
+        //    {
+        //        System.Console.WriteLine("Hello");
+        //        _logic.AddBox(10, 30, 2000);
+        //        _logic.AddBox(10, 30, 3000);
+        //        _logic.AddBox(20, 10, 1400);
+        //        _logic.AddBox(10, 30, 5030);
+        //        _logic.AddBox(20, 10, 4400);
+        //        _logic.AddBox(20, 20, 3200);
+        //        _logic.AddBox(20, 25, 8200);
+        //        _logic.AddBox(5, 10, 4430);
+        //        _logic.AddBox(5, 5, 5555);
+        //        _logic.AddBox(5, 15, 1120);
+        //        _logic.AddBox(15, 15, 2048);
+        //        _logic.AddBox(15, 20, 1024);
+        //        _logic.AddBox(50, 20, 4096);
+        //        BoxInfo NeededInfo;
+        //        _logic.Info(5, 5, out NeededInfo);
+        //        Console.ReadLine();
+        //    }
+        //}
+        //
+    }
+}
